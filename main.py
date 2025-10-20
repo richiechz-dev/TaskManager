982276c6d637adcab7f48b3de64678844dab90de
Ricardo Chavez <gic.rc455@gmail.com>
2025-10-20 15:38:03 -0600

    Creacion de Menu

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def main():
     print("Hello from taskmanager!")
+    print("Menu Principal\n")
+    print("1. Agregar una nueva tarea")
+    print("2. Marcar tarea como completada")
+    print("3. Mostrar todas las tareas")
+    print("4. Salir()")
+
+    opc = int(input("Selecciona una opcion: "))
 
 
 if __name__ == "__main__":
