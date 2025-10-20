4a2c870e29be520aef292482f1c1109bfbee8e18
Ricardo Chavez <gic.rc455@gmail.com>
2025-10-20 15:18:48 -0600

    WIP: Creacion de la clase TaskManager, trabajando en metodos

---
+++ gestor/manager.py
@@ -0,0 +1,18 @@
+'''Crear una clase
+
+Debe contener 4 funciones 
+1. Agregar nueva tarea
+2. Marcar como completada
+3. Mostrar todas las tareas
+4. Eliminar tarea
+'''
+
+class TaskManager:
+    
+    # Este es el constructor que va a inicilizarse cada vez que se cree una instancia de la clase TaskManager
+    def __init__(self):
+        pass
+    
+    # Creamos las funciones de la clase
+    def add_task(self):
+        pass
