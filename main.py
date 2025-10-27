3183a25760a8c8513963aca1d63f00c48f1f5951
Ricardo Chavez <gic.rc455@gmail.com>
2025-10-27 11:20:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from gestor.manager import TaskManager  # Importamos la clase TaskManager
+
+
 def menu():  # Envolvemos el menu dentro de una funcion por buenas practicas
     print("Hello from taskmanager!")
     print("Menu Principal\n")
@@ -8,17 +11,20 @@ def menu():  # Envolvemos el menu dentro de una funcion por buenas practicas
 
 
 def main():
+    task_manager = TaskManager()  # Creamos una instancia de la clase o un objeto
     menu()
     while True:  # Repite lo siguente para siempre
         try:
             opc = int(input("Selecciona una opcion: "))  # Ingresa una opción
             # Va a comparar en el siguiente orden si una opc coincide con la ingresada
             if opc == 1:
-                pass
+                description = str(input("Description: "))
+                task_manager.add_task(description)
             elif opc == 2:
-                pass
+                id_task = int(input("id: "))
+                task_manager.complete_task(id_task)
             elif opc == 3:
-                pass
+                task_manager.show_tasks()
             elif (
                 opc == 4
             ):  # Si coincide con la opcion 4 el programa finalizara con break y terminara el bucle
